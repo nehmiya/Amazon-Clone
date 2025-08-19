44489f65e96cfce7da25795fb7465a6a58faf707
Nehmiya <nehmiyatech@gmail.com>
2025-08-19 21:46:35 +0300

    Added img err handler for broken api img endpoints

--- Client/src/Components/Product/ProductCard.jsx
+++ Client/src/Components/Product/ProductCard.jsx
@@ -7,17 +7,30 @@ function ProductCard({ product }) {
   return (
     <div className={classes.card__container}>
       <a href={product.id}>
-        <img src={product.image} alt="" />
+        <img
+          src={product.image}
+          alt={product.title}
+          onError={(e) => {
+            if (e.target.src.endsWith(".jpg")) {
+              e.target.src = e.target.src.replace(".jpg", "t.png");
+            } else {
+              e.target.src =
+                "https://via.placeholder.com/250x250?text=No+Image";
+            }
+          }}
+        />
       </a>
       <div>
         <h3>{product.title}</h3>
         <div className={classes.rating}>
           {/* Rating */}
-          <Rating readOnly value={product.rating.rate}/>
+          <Rating readOnly value={product.rating.rate} />
           {/* Count */}
           <small>{product.rating.count}</small>
         </div>
-        <div><CurrencyFormat amount={product.price}/></div>
+        <div>
+          <CurrencyFormat amount={product.price} />
+        </div>
         <button className={classes.button}>Add To Cart </button>
       </div>
     </div>
